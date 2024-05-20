9130520673ce019000031297dc7bee16bf2ff838
SrjAdhikari <surajadhikari055@gmail.com>
2024-05-20 10:13:06 +0900

    Basics Of Programming - 2

--- 02_Basics_Of_Programming-2/02_Pattern Printing-2/Codes/printFancyPattern2.cpp
+++ 02_Basics_Of_Programming-2/02_Pattern Printing-2/Codes/printFancyPattern2.cpp
@@ -1,26 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printFancyPattern(int n){
+void printFancyPattern(int n)
+{
 
-    for(int row = 0; row < n; row++){
-        if(row == 0){
+    for (int row = 0; row < n; row++)
+    {
+        if (row == 0)
+        {
             cout << row + 1;
         }
-        else if(row == n - 1){
-            for(int col = 0; col < n; col++){
+        else if (row == n - 1)
+        {
+            for (int col = 0; col < n; col++)
+            {
                 cout << col + 1 << " ";
             }
         }
-        else{
-            for(int col = 0; col < row + 2; col++){
-                if(col == 0){
+        else
+        {
+            for (int col = 0; col < row + 2; col++)
+            {
+                if (col == 0)
+                {
                     cout << col + 1;
                 }
-                else if(col == (row + 2 - 1)){
+                else if (col == (row + 2 - 1))
+                {
                     cout << col << " ";
                 }
-                else{
+                else
+                {
                     cout << "  ";
                 }
             }
@@ -29,12 +39,13 @@ void printFancyPattern(int n){
     }
 }
 
-int main(){
+int main()
+{
 
     int row;
 
     cout << "Enter the number of row : " << endl;
-    cin >> row; 
+    cin >> row;
 
     printFancyPattern(row);
 
