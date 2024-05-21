112591f295be08019ab1497ecba199e28229dc73
SrjAdhikari <surajadhikari055@gmail.com>
2024-05-22 00:05:18 +0900

    Array Level - 1

---
+++ 03_Array/Array_Level-1/Codes/findUniqueElement.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int findUniqueElement(int arr[], int size){
+
+    int uniqueNum = 0;
+
+    for(int i = 0; i < size; i++){
+        uniqueNum ^= arr[i];
+    }
+    return uniqueNum;
+}
+
+
+int main(){
+
+    int arr[] = {4, 1, 2, 1, 2};
+    int size = 5;
+
+    int ans  = findUniqueElement(arr,size);
+    cout << "Unique number is : " << ans << endl;
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
